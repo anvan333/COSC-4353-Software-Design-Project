1a8c205bd58417b03a2c5df0eaeb82325d4bc4ec
An <thienanvan@gmail.com>
2022-05-05 00:31:24 -0500

    db commit

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/css'))
 app.use('/imgs', express.static(__dirname + 'public/css'))
 
+const urilink = ('mongodb+srv://cluster0.8msb7.mongodb.net/myGroup32db?appName=mongosh+1.3.1');
+mongoose.connect(urilink, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
 
 //set views
 app.set('views', './views')
@@ -43,6 +50,11 @@ app.get('/fuelhistory', (req, res) => {
 app.get('/fuelquote', (req, res) => {
     res.render('fuel_quote')
 })
+
+// app.get('/profile', (req, res) => {
+//     res.render('fuel_quote')
+// })
+
 
 
 //listen on port 3000
